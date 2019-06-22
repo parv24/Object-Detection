6a6a8964720193e8f43d9223157aeeb2475a9186
Parvesh Garg <parveshg85@gmail.com>
2019-06-22 17:12:09 +0530

    Update red_object_detection.py

--- red_object_detection.py
+++ red_object_detection.py
@@ -8,23 +8,10 @@ cap = cv2.VideoCapture(0)   #0: for webcam, 1: for external camera
 #reading from file
 #cap = cv2.VideoCapture("video1.mp4")   #0: for webcam, 1: for external camera
 cv2.namedWindow("thresholding",cv2.WINDOW_NORMAL)
-# creating resizable window for video output
-'''cv2.createTrackbar('H_LOW','thresholding',0,180,nothing)
-cv2.createTrackbar('H_HIGH','thresholding',180,180,nothing)
-cv2.createTrackbar('S_LOW','thresholding',0,255,nothing)
-cv2.createTrackbar('S_HIGH','thresholding',255,255,nothing)
-cv2.createTrackbar('V_LOW','thresholding',0,255,nothing)
-cv2.createTrackbar('V_HIGH','thresholding',255,255,nothing)'''
 while cap.isOpened():
     _, img = cap.read()
    # cv2.imshow("VIDEO FEED",img)
     frame= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    #r = cv2.getTrackbarPos('H_LOW','thresholding')
-    #R = cv2.getTrackbarPos('H_HIGH','thresholding')
-    #g = cv2.getTrackbarPos('S_LOW','thresholding')
-    #G = cv2.getTrackbarPos('S_HIGH','thresholding')
-    #b = cv2.getTrackbarPos('V_LOW','thresholding')
-    #B = cv2.getTrackbarPos('V_HIGH','thresholding')
     lower = (100,100,20)
     higher = (255,255,179)
     gaussian = cv2.GaussianBlur(frame,(5,5),0)
@@ -37,7 +24,9 @@ while cap.isOpened():
 
     k = cv2.waitKey(10) & 0xFF          #waiting for user input
     if frame is None or k == ord('q'):  #if user quits
-        cv2.destroyAllWindows()         #destroy all windows
+        cv2.destroyAllWindows()    
+        cap.release()
+        #destroy all windows
         break
 
         #output the image
