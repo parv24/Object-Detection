c2bfd3d796a20c6149e4178850d48b202cc084e8
Parvesh Garg <parveshg85@gmail.com>
2019-06-22 17:11:15 +0530

    Update tracking_red_object.py

--- tracking_red_object.py
+++ tracking_red_object.py
@@ -2,37 +2,18 @@ import cv2
 import numpy as np
 def nothing(x):
     pass
-# creating object
 cap = cv2.VideoCapture(0)   #0: for webcam, 1: for external camera
-
-#reading from file
-#cap = cv2.VideoCapture("video1.mp4")   #0: for webcam, 1: for external camera
-
-# creating resizable window for video output
-'''cv2.createTrackbar('H_LOW','thresholding',0,180,nothing)
-cv2.createTrackbar('H_HIGH','thresholding',180,180,nothing)
-cv2.createTrackbar('S_LOW','thresholding',0,255,nothing)
-cv2.createTrackbar('S_HIGH','thresholding',255,255,nothing)
-cv2.createTrackbar('V_LOW','thresholding',0,255,nothing)
-cv2.createTrackbar('V_HIGH','thresholding',255,255,nothing)'''
 while cap.isOpened():
     _, img = cap.read()
-   # cv2.imshow("VIDEO FEED",img)
-   # blur = cv2.pyrMeanShiftFiltering(img,21,51)
     blur = cv2.GaussianBlur(img,(5,5),0)
     frame= cv2.cvtColor(blur,cv2.COLOR_BGR2HSV)
-    #r = cv2.getTrackbarPos('H_LOW','thresholding')
-    #R = cv2.getTrackbarPos('H_HIGH','thresholding')
-    #g = cv2.getTrackbarPos('S_LOW','thresholding')
-    #G = cv2.getTrackbarPos('S_HIGH','thresholding')
-    #b = cv2.getTrackbarPos('V_LOW','thresholding')
-    #B = cv2.getTrackbarPos('V_HIGH','thresholding')
     lower = (100,100,20)
     higher = (255,255,179)
     #gaussian = cv2.GaussianBlur(frame,(5,5),0)
     thresh = cv2.inRange(frame,lower,higher)
     # find contours in the thresholded image
-    cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+    _, cnts, _= cv2.findContours(thresh.copy(), cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+    type(cnts)
     max_area = 20
     best_cnt = 1
     for cnt in cnts:
@@ -56,6 +37,7 @@ while cap.isOpened():
     k = cv2.waitKey(10) & 0xFF          #waiting for user input
     if frame is None or k == ord('q'):  #if user quits
         cv2.destroyAllWindows()         #destroy all windows
+        cap.release()
         break
 
         #output the image
